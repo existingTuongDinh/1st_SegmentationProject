bba2f35ddbea69d05b8303539b479d1ef8a82528
existingTuongDinh <118448833+existingTuongDinh@users.noreply.github.com>
2023-06-23 10:39:18 +0700

    Add files via upload

---
+++ SegProj/SegProj/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace SegProj
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblInput = new Label();
+            lblOutput = new Label();
+            pictureBox1 = new PictureBox();
+            pictureBox2 = new PictureBox();
+            lblThreshold = new Label();
+            txtboxThreshold = new TextBox();
+            label1 = new Label();
+            lblX1 = new Label();
+            lblY1 = new Label();
+            lblX2 = new Label();
+            lblY2 = new Label();
+            textBoxX1 = new TextBox();
+            txtboxY1 = new TextBox();
+            txtboxX2 = new TextBox();
+            txtboxY2 = new TextBox();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // lblInput
+            // 
+            lblInput.AutoSize = true;
+            lblInput.Location = new Point(25, 32);
+            lblInput.Name = "lblInput";
+            lblInput.Size = new Size(35, 15);
+            lblInput.TabIndex = 0;
+            lblInput.Text = "Input";
+            // 
+            // lblOutput
+            // 
+            lblOutput.AutoSize = true;
+            lblOutput.Location = new Point(355, 32);
+            lblOutput.Name = "lblOutput";
+            lblOutput.Size = new Size(45, 15);
+            lblOutput.TabIndex = 1;
+            lblOutput.Text = "Output";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Location = new Point(25, 69);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(324, 412);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+  
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Location = new Point(355, 69);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(357, 412);
+            pictureBox2.TabIndex = 3;
+            pictureBox2.TabStop = false;
+            // 
+            // lblThreshold
+            // 
+            lblThreshold.AutoSize = true;
+            lblThreshold.Location = new Point(967, 92);
+            lblThreshold.Name = "lblThreshold";
+            lblThreshold.Size = new Size(51, 15);
+            lblThreshold.TabIndex = 4;
+            lblThreshold.Text = "Ngưỡng";
+            // 
+            // txtboxThreshold
+            // 
+            txtboxThreshold.Location = new Point(891, 110);
+            txtboxThreshold.Name = "txtboxThreshold";
+            txtboxThreshold.Size = new Size(211, 23);
+            txtboxThreshold.TabIndex = 5;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(869, 178);
+            label1.Name = "label1";
+            label1.Size = new Size(253, 15);
+            label1.TabIndex = 6;
+            label1.Text = "Chọn vùng ảnh để tính vecto trung binh mau :";
+            // 
+            // lblX1
+            // 
+            lblX1.AutoSize = true;
+            lblX1.Location = new Point(837, 232);
+            lblX1.Name = "lblX1";
+            lblX1.Size = new Size(20, 15);
+            lblX1.TabIndex = 7;
+            lblX1.Text = "X1";
+            // 
+            // lblY1
+            // 
+            lblY1.AutoSize = true;
+            lblY1.Location = new Point(837, 302);
+            lblY1.Name = "lblY1";
+            lblY1.Size = new Size(20, 15);
+            lblY1.TabIndex = 8;
+            lblY1.Text = "Y1";
+            // 
+            // lblX2
+            // 
+            lblX2.AutoSize = true;
+            lblX2.Location = new Point(1007, 232);
+            lblX2.Name = "lblX2";
+            lblX2.Size = new Size(20, 15);
+            lblX2.TabIndex = 9;
+            lblX2.Text = "X2";
+            // 
+            // lblY2
+            // 
+            lblY2.AutoSize = true;
+            lblY2.Location = new Point(1007, 307);
+            lblY2.Name = "lblY2";
+            lblY2.Size = new Size(20, 15);
+            lblY2.TabIndex = 10;
+            lblY2.Text = "Y2";
+            // 
+            // textBoxX1
+            // 
+            textBoxX1.Location = new Point(863, 229);
+            textBoxX1.Name = "textBoxX1";
+            textBoxX1.Size = new Size(100, 23);
+            textBoxX1.TabIndex = 11;
+            // 
+            // txtboxY1
+            // 
+            txtboxY1.Location = new Point(863, 299);
+            txtboxY1.Name = "txtboxY1";
+            txtboxY1.Size = new Size(100, 23);
+            txtboxY1.TabIndex = 12;
+            // 
+            // txtboxX2
+            // 
+            txtboxX2.Location = new Point(1033, 229);
+            txtboxX2.Name = "txtboxX2";
+            txtboxX2.Size = new Size(100, 23);
+            txtboxX2.TabIndex = 13;
+            // 
+            // txtboxY2
+            // 
+            txtboxY2.Location = new Point(1033, 302);
+            txtboxY2.Name = "txtboxY2";
+            txtboxY2.Size = new Size(100, 23);
+            txtboxY2.TabIndex = 14;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.ButtonFace;
+            button1.Location = new Point(945, 387);
+            button1.Name = "button1";
+            button1.Size = new Size(120, 94);
+            button1.TabIndex = 15;
+            button1.Text = "Phân đoạn ảnh segmentation";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1167, 512);
+            Controls.Add(button1);
+            Controls.Add(txtboxY2);
+            Controls.Add(txtboxX2);
+            Controls.Add(txtboxY1);
+            Controls.Add(textBoxX1);
+            Controls.Add(lblY2);
+            Controls.Add(lblX2);
+            Controls.Add(lblY1);
+            Controls.Add(lblX1);
+            Controls.Add(label1);
+            Controls.Add(txtboxThreshold);
+            Controls.Add(lblThreshold);
+            Controls.Add(pictureBox2);
+            Controls.Add(pictureBox1);
+            Controls.Add(lblOutput);
+            Controls.Add(lblInput);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+    
+
+        #endregion
+
+        private Label lblInput;
+        private Label lblOutput;
+        private PictureBox pictureBox1;
+        private PictureBox pictureBox2;
+        private Label lblThreshold;
+        private TextBox txtboxThreshold;
+        private Label label1;
+        private Label lblX1;
+        private Label lblY1;
+        private Label lblX2;
+        private Label lblY2;
+        private TextBox textBoxX1;
+        private TextBox txtboxY1;
+        private TextBox txtboxX2;
+        private TextBox txtboxY2;
+        private Button button1;
+    }
+}
---
+++ SegProj/SegProj/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace SegProj
+{
+    public partial class Form1 : Form
+    {
+        //load hinh goc 
+        Bitmap HinhGoc = new Bitmap("C:\\Users\\84923\\Pictures\\640px-thumbnail.jpg");
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            //hien thi len PicBox
+            pictureBox1.Image = HinhGoc;
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom; // Thay đổi thuộc tính SizeMode cho pictureBox1
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //lay du lieu va chuyen du lieu string thanh du lieu so de thuat toan co the hieu
+            int x1 = Convert.ToInt16(textBoxX1.Text); // co 4 toa do de tao ra cai vung , 2 toa do chi la duong thang
+            int x2 = Convert.ToInt16(txtboxX2.Text);
+            int y1 = Convert.ToInt16(txtboxY1.Text);
+            int y2 = Convert.ToInt16(txtboxY2.Text);
+            int Nguong = Convert.ToInt16(txtboxThreshold.Text);
+            
+
+            //khai bao bien so thuc , bat dau = 0 de tinh gia tri cua red light , green light va blue light
+            double aRtb = 0, aGtb = 0, aBtb = 0;
+
+            //tinh vecto mau trung binh ( average vector color )
+            for (int x = x1; x <= x2; x++) // duyet qua cac pixel thuoc vung anh da chon
+                for (int y = y1; y <= y2; y++)
+                {
+                    Color pixel = HinhGoc.GetPixel(x, y);
+                    aRtb += pixel.R; //vecto a= [aR aG aB] , chua ba thanh phan trung binh mau cho tung kenh R G B
+                    aGtb += pixel.G; // lay mau cho tung kenh
+                    aBtb += pixel.B;
+                }
+
+            //double size ((x2-x1+1)*(y2-y1+1))
+            //tai moi kenh mau R,G,B tinh trung binh cong
+            double Size = Math.Abs((x2 - x1) * (y2 - y1)); // tinh tong so luong pixel trong vung anh da chon
+            aRtb /= Size;
+            aGtb /= Size;
+            aBtb /= Size;
+
+            //phan doan anh
+            //tao mot anh bitmap chua hinh segmentation
+            Bitmap SegImg = new Bitmap(HinhGoc.Width, HinhGoc.Height); //Bitmap la mot lop trong thu vien System.Drawing de luu tru anh duoi dang pixel
+            //vecto z la diem anh tai vi tri x,y ma chung ta muon tinh xem no thuoc nen ( back ground ) hay thuoc doi tuong (object)
+            for (int x = 0; x < HinhGoc.Width; x++) //vong lap nay duyet qua tat ca cac pixel trong anh HinhGoc
+                for (int y = 0; y < HinhGoc.Height; y++)
+                {
+                    Color Pixel2 = HinhGoc.GetPixel(x, y); //doi tuong color dung de dai dien cho mau trong tung pixel cua HinhGoc
+                    double zR = Pixel2.R;
+                    double zG = Pixel2.G;   // cac thanh phan mau R,G,B duoc lay ra va luu tru vao cac bien duoc khoi tao
+                    double zB = Pixel2.B;
+
+                    //ap dung cong thuc 6.7-1 de tinh Euclidean Distance giua 2 vecto a va z se nhu sau : 
+                    double D = Math.Sqrt(Math.Pow(zR - aRtb, 2) + Math.Pow(zG - aGtb, 2) + Math.Pow(zB - aBtb, 2)); // de tinh su khac nhau giua R,G,B cua phan vung va hinh goc
+
+                    //sau khi tinh duoc D(a,z) , ta se so sanh voi gia tri nguong (threshold) 
+                    //D0 de xac dinh xem diem z(x,y) dang xet la background hay object
+                    if (Convert.ToInt32(D) >= Nguong) //la background , set cho mau cac diem nay ve mau trang , //khi D la double muon chuyen ve int phai convert.to.int32
+                        SegImg.SetPixel(x, y, Color.FromArgb((int)zR, (int)zG, (int)zB));
+                    else //nguoc lai z(x,y) la object thi giu nguyen mau
+                        SegImg.SetPixel(x, y, Color.FromArgb(255, 255, 255));
+                    //cong thuc tren chu yeu de xac dinh do tuong dong gia tri anh sang mau giua phan vung va gia tri trung binh cua pixel mau , de xac dinh thuoc ve background hay object , tu do xu li phan loai anh
+                }
+            //hien thi anh da segmentation
+            pictureBox2.Image = SegImg;
+        }
+
+        
+    }
+}
